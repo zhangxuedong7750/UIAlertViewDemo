cc32e74af6bd8373e658b1f0c4944377344c66f5
张雪东 <zhangxuedong@zhangxuedongdeiMac.local>
2016-04-14 10:00:38 +0800

    Init

---
+++ UIAlertViewDemo/BlockAlertView.swift
@@ -0,0 +1,13 @@
+//
+//  BlockAlertView.swift
+//  UIAlertViewDemo
+//
+//  Created by 张雪东 on 16/4/14.
+//  Copyright © 2016年 张雪东. All rights reserved.
+//
+
+import Cocoa
+
+class BlockAlertView: UIAlertView {
+
+}
